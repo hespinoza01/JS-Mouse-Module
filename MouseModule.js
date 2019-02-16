97f00515079954c30905286bb500c9d3ac28675b
Harold Espinoza <haroldesptru@gmail.com>
2019-02-16 13:16:04 -0600

    Añadido evento 'mousemove' al body para capturar los valores de l posición del mouse y hacer los calculos de los demás valores.

--- MouseModule.js
+++ MouseModule.js
@@ -2,16 +2,38 @@
 
 const mouse = function(){
   let _mouse = {
-    current: { x: 0, y: 0 },
-    memory: { x: 0, y: 0 },
-    difference: { x: 0, y: 0 },
-    inverse: { x: 0, y: 0 },
-    dragging: false
+    current: { x: 0, y: 0 },     // current position on screen
+    memory: { x: 0, y: 0 },      // save start mouse position on screen before start drag
+    difference: { x: 0, y: 0 },  // difference between current and memory position
+    inverse: { x: 0, y: 0 },     // handle negative distance
+    dragging: false              // detect dragging state
   };
+
+
+  // Capture the mouse current position when it's move
+  document.body.addEventListener('mousemove', e => {
+    // current position
+    _mouse.current.x = e.pageX;
+    _mouse.current.y = e.pageY;
+
+
+    if(_mouse.dragging) {
+      // calc the difference between memory and current
+      _mouse.difference.x = _mouse.current.x - _mouse.memory.x;
+      _mouse.difference.y = -_mouse.current.y - _mouse.memory.y;
+
+
+      // calc inverses values
+      if(_mouse.current.x < _mouse.memory.x)
+        _mouse.inverse.x = _mouse.current.x;
+
+      if(_mouse.current.y < _mouse.memory.y)
+        _mouse.inverse.y = _mouse.current.y;
+    }
+  });
+
 
   return _mouse;
 };
 
 window.Mouse = Object.create(mouse());
-
-console.log(Mouse.current);
